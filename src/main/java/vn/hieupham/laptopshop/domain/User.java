8df4dcefee9aa62df0dd1a0cf3c4702901d54f46
HieuPahm-MVP <phamvanthang786@gmail.com>
2024-12-05 18:05:03 +0700

    5/12/2024

--- src/main/java/vn/hieupham/laptopshop/domain/User.java
+++ src/main/java/vn/hieupham/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package vn.hieupham.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +23,17 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+    //connect role id
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    //connect order
+    @OneToMany(mappedBy = "user")
+    List<Order> Orders;
+    
     public long getId() {
         return id;
     }
@@ -52,8 +70,16 @@ public class User {
     public void setPhone(String phone) {
         this.phone = phone;
     }
-    @Override
-    public String toString(){
-        return "User [id = " + id + ", email = " + email +", password = " + password + ", fullName = "+fullName+ ",address = "+address+", phone = "+phone+"]";
+    public String getAvatar() {
+        return avatar;
     }
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
+    }
+    
 }
--- src/main/java/vn/hieupham/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hieupham/laptopshop/repository/UserRepository.java
@@ -3,7 +3,7 @@ package vn.hieupham.laptopshop.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 
 import vn.hieupham.laptopshop.domain.User;
